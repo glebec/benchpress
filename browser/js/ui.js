0311b8ba964f2bc31bfc91affce46e48bde4a43a
Gabriel Lebec <glebec@gmail.com>
2017-01-11 11:26:37 -0500

    feat(ui): progress indicator

--- browser/js/ui.js
+++ browser/js/ui.js
@@ -1,15 +1,30 @@
-const tableBodyElem = document.querySelector('#stats tbody');
 
-function addStatRow (stats) {
-  const row = document.createElement('tr');
+const uiModule = (function () {
 
-  row.innerHTML = `
-    <td>${stats.numItems}</td>
-    <td>${stats.algorithm}</td>
-    <td>${stats.msPerOp}</td>
-    <td>&#177;${stats.rme}%</td>
-    <td>${stats.samples}</td>
-  `;
+  const tableBodyElem = document.querySelector('#stats tbody');
+  const loadingDiv = document.querySelector('#loading');
 
-  tableBodyElem.appendChild(row);
-}
+  function addStatRow (stats) {
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td>${stats.numItems}</td>
+      <td>${stats.algoName}</td>
+      <td>${stats.msPerOp}</td>
+      <td>&#177;${stats.rme}%</td>
+      <td>${stats.samples}</td>
+    `;
+
+    tableBodyElem.appendChild(row);
+  }
+
+  function stopLoading () {
+    loadingDiv.innerHTML = '';
+  }
+
+  return {
+    addStatRow,
+    stopLoading
+  };
+
+}());
